dcefde3cfd4e53172cd7d0a298dea723c3fd6ddf
AJC <AlexCast001@gmail.com>
2024-04-27 20:28:30 +0000

    start fsm skeleton

---
+++ fsm/fsm.py
@@ -0,0 +1,14 @@
+"""
+Purpose:
+Sandbox playground for FSM (finite state machine) implementations
+in python.
+
+Instructions:
+TBD
+"""
+
+def main():
+    raise NotImplementedError("Pending Implementation")
+
+if __name__ == "__main__":
+    main()
